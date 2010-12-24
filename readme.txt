a6e057f2230e0d30c8bdc63fa2c64b6fb67696d2
sajib1223 <sajib1223@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-24 16:53:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/w4-internal-link-shortcode/tags/1.1@325925 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,14 +1,21 @@
-=== Plugin Name ===
+=== W4 Internal Link Shortcode ===
 Plugin Name: W4 Internal Link Shortcode
 Author: sajib1223, Shazzad Hossain Khan
 Donate link: http://w4dev.com/
-Tags: links, shortcode, post, page, category
+Tags: links, shortcode, post links, page links, category links, author links
 Requires at least: 2.9
 Tested up to: 3.0.3
-Stable tag: 1.0
+Stable tag: 1.1
+
+Wordpress plugin for putting internal links with shortcode..
 
 == Description ==
 Now you can easily embed your wordpress sites internal links (of post,page,category,author) in post,page or category description pages. Just write the appropriate shortcode,and you are on. No need to update these links whenever you make any changes to the real link location.
+
+
+= Upgrade Notice =
+* New attribute ("before" and "after") introduced in Version 1.1. These are for putting texts on before or after the link.
+* Added similar parameter for attributes "type". Like: you can use "p" for "post", "c" for "cat", "a" for "author" and few more..
 
 = Example: =
 
@@ -16,9 +23,6 @@ The default category link of your site should look like something http://example
 <pre>[intlink type="cat" slug="uncategorized"]</pre>
 
 Now, if you change your category base to cats, like http://example.com/cats/uncategorized/ , you don't have to update the shortcode. It will take the latest format to show the link.
-
-
-= Use shortcode "intlink" to put a link. =
 
 = Arguments: =
 
@@ -28,14 +32,19 @@ Now, if you change your category base to cats, like http://example.com/cats/unca
 * name = default ''. (Name of the link item object. Ex: "Uncategorized" for category Uncategorized, "Hello world" for the post Hello world).
 * slug = default ''. (Slug of the link item object. Ex: "hello-world" for the post Hello world).
 * class = default 'w4ils_link'. (A HTML class for the link element "a").
+* before = default ''. (Put text or html element such as "<br />" or "<p>" before the link).
+* after = default ''. (Put text or html element after the link).
+
 
 = Notes =
+
 * Remember,you have to write the shortcode properly. Script has a priority for finding a link from your shortcode.
 * Firstly, the script will try to find the link type. if you haven't entered any type in your shortcode, it will assume the link as post/page link.
 * Secondly, it will find your given attribute(id,name,slug) for the link. By priority, it will look for the "id" first. if id not found,then it will look for the "name". And last it will look for the slug.Slug is pretty eiser than name,as it contains all small letter and no space. If the is a name "I love Wordpress",the slug should be for this "i-love-wordpress",there is some exception although.
-* Lastly, it will fletch the link based on the matched attribute.
+* Lastly, it will fletch the link based on the matched attribute and its parameter.
 
-= Please let us know if you are unable to use/manage this plugin or if you have some suggestions to improve this plugin. <a href="http://w4dev.com/w4-plugin/w4-internal-link-shortcode/">Contact W4 development</a> =
+Please let us know if you are unable to use/manage this plugin or if you have some suggestions to improve this plugin.
+
 
 == Installation ==
 1. Upload plugin zip file to the `/wp-content/plugins/` directory
@@ -50,21 +59,25 @@ You can use it in post, page and in category description field.
 == Screenshots ==
 
 == Changelog ==
-= 1.0 =
-
+= 1.1 =
+New attribute ("before" and "after") introduced for putting texts on before or after the link.
 
 
 == Upgrade Notice ==
-= 1.0 =
+= 1.1 =
+New attribute ("before" and "after") introduced for putting texts on before or after the link.
 
 
 == Usages ==
-Use shortcode "intlink" to put a link.
+= Use shortcode "intlink" to put a link. =
 
-Arguments
-1. type = default 'post'. (your link type:post/page/cat/author).
-2. id = default ''. (this is the post/page/author/category id based on the type you have chosen).
-3. text = default based on the type. Post/Page title for type 'post' or 'page', Category name for 'cat', author display name for 'author'. (the text to show inside the link).
-4. name = default ''. (Name of the link item object. Ex: "Uncategorized" for category Uncategorized, "Hello world" for the post Hello world).
-5. slug = default ''. (Slug of the link item object. Ex: "hello-world" for the post Hello world).
-6. class = default 'w4ils_link'. (A HTML class for the link element "a").
+= Arguments: =
+
+* type = default 'post'. (your link type:post/page/cat/author).
+* id = default ''. (this is the post/page/author/category id based on the type you have chosen).
+* text = default based on the type. Post/Page title for type 'post' or 'page', Category name for 'cat', author display name for 'author'. (the text to show inside the link).
+* name = default ''. (Name of the link item object. Ex: "Uncategorized" for category Uncategorized, "Hello world" for the post Hello world).
+* slug = default ''. (Slug of the link item object. Ex: "hello-world" for the post Hello world).
+* class = default 'w4ils_link'. (A HTML class for the link element "a").
+* before = default ''. (Put text or html element such as "<br />" or "<p>" before the link).
+* after = default ''. (Put text or html element after the link).
--- w4-internal-link-shortcode.php
+++ w4-internal-link-shortcode.php
@@ -3,138 +3,168 @@
 Plugin Name: W4 Internal Link Shortcode
 Plugin URI: http://w4dev.com/w4-plugin/w4-internal-link-shortcode/
 Description: Lets you embed links in a post/page/category description of any post,page,category and author url.
-Version: 1.0
+Version: 1.1
 Author: Shazzad Hossain Khan
 Author URI: http://w4dev.com/
 */
 define( 'W4ILS_DIR', plugin_dir_path(__FILE__)) ;
 define( 'W4ILS_URL', plugin_dir_url(__FILE__)) ;
 define( 'W4ILS_BASENAME', plugin_basename( __FILE__ )) ;
-define( 'W4ILS_VERSION', '1.0' ) ;
+define( 'W4ILS_VERSION', '1.1' ) ;
 define( 'W4ILS_NAME', 'W4 Internal Link Shortcode' ) ;
 define( 'W4ILS_SLUG', strtolower(str_replace(' ', '-', W4ILS_NAME ))) ;
 
 
-//HOOKS
-add_filter('the_content', 'w4ils_replace_callback' ) ;
-add_filter('the_excerpt', 'w4ils_replace_callback' ) ;
-add_filter('get_the_content', 'w4ils_replace_callback' ) ;
-add_filter('get_the_excerpt', 'w4ils_replace_callback' ) ;
-add_filter('category_description',  'w4ils_replace_callback' ) ;
-add_action( 'plugin_action_links_'.W4ILS_BASENAME, 'w4ils_plugin_action_links' ) ;
-
-
-function w4ils_plugin_action_links( $links ){
-	$readme_link['readme'] = '<a href="http://w4dev.com/w4-plugin/w4-internal-link-shortcode/">'.esc_html( __( 'How to use' )).'</a>';
-	return array_merge( $links, $readme_link );
-}
-//Retrive and replace links
-function w4ils_replace( $matches ){
-	//print_r(shortcode_parse_atts($matches[1]));
+class W4ILS{
 	
-	$w4ils_default = array( 'id' => null, 'type' => 'post', 'name' => '', 'slug' => '', 'text' => '', 'class' => 'w4ils_link' );
-	$w4ils_args = shortcode_parse_atts($matches[1]);
-	$w4ils_args = wp_parse_args( $w4ils_args, $w4ils_default);
-	
-	//print_r($w4ils_args);
-	$w4ils_default_types = array( 'post', 'page', 'cat', 'author' ) ;
-	extract($w4ils_args, EXTR_SKIP) ;
-	if(!in_array($type, $w4ils_default_types)){
-		$type = 'post' ;
+	function W4ILS(){
+		add_filter('the_content', array(&$this, 'w4ils_replace_callback'));
+		add_filter('the_excerpt', array(&$this, 'w4ils_replace_callback'));
+		add_filter('get_the_content', array(&$this, 'w4ils_replace_callback'));
+		add_filter('get_the_excerpt', array(&$this, 'w4ils_replace_callback'));
+		add_filter('category_description', array(&$this, 'w4ils_replace_callback'));
+		add_action('plugin_action_links_'.W4ILS_BASENAME, array(&$this, 'w4ils_plugin_action_links'));
 	}
 	
-	if($name){
-		$name = stripslashes( $name ) ;
+	function w4ils_plugin_action_links( $links ){
+		$readme_link['readme'] = '<a href="http://w4dev.com/w4-plugin/w4-internal-link-shortcode/">'.esc_html( __( 'How to use' )).'</a>';
+		return array_merge( $links, $readme_link );
 	}
+
+	//Retrive and replace links
+	function w4ils_replace( $matches ){
+		//Supported attributes
+		$w4ils_default_attr = array(
+							'type' 		=> 'post',
+							'id' 		=> '',
+							'name'	 	=> '',
+							'slug' 		=> '',
+							'text' 		=> '',
+							'class' 	=> 'w4ils_link',
+							'before' 	=> '',
+							'after' 	=> ''
+							);
+		//Parsing attributes from shortcode by wp method
+		$w4ils_attr = shortcode_parse_atts($matches[1]);
+		$w4ils_attr = wp_parse_args( $w4ils_attr, $w4ils_default_attr);
+		
+		//Supported link types
+		$w4ils_default_types = array(
+								'post' => array('p','post','posts','page','pages'),
+								'cat' => array('c','cat','cats','category'),
+								'author' => array('a','auth','aothor','author')
+								);
+		$w4ils_all_types = array_merge($w4ils_default_types['post'], $w4ils_default_types['cat'], $w4ils_default_types['author']);
+		
+		
+		extract($w4ils_attr, EXTR_SKIP) ;
+
+		if(!in_array($type, $w4ils_all_types))
+			$type = 'post' ;
+		
+		$type = strtolower($type);
+
+		if($name)
+			$name = stripslashes($name);
+
+		if(in_array($type, $w4ils_default_types['post'])){
+			if(!$w4post = $this->get_post_by_field($id, 'ID')){
+				//Get post/page by title
+				$w4post = $this->get_post_by_field($name);
 	
-	if($text){
-		$text = stripslashes( $text ) ;
-	}
-	//print_r($name);
-	if($type == 'post' || $type == 'page'){
-		if(!$w4post = w4ils_get_post_by_field($id, 'ID')){
+				//Get post/page by name/slug
+				if(!$w4post)
+					$w4post = $this->get_post_by_field($slug, 'post_name');
+				
+				$id = $w4post->ID;
+				if(!get_post($id))
+					return false ;
+			}
 
-			//Get post/page by title
-			$w4post = w4ils_get_post_by_field($name);
-
-			//Get post/page by name/slug
-			if(!$w4post)
-				$w4post = w4ils_get_post_by_field($slug, 'post_name');
-			
-			$id = $w4post->ID;
-			if(!$id)
-				return false ;
+			$link = get_permalink($id);
+			if(!$text)
+				$text = get_the_title($id) ;
 		}
-		$link = get_permalink($id);
-		if(!$text) $text = get_the_title($id) ;
-	}
+		
+		if(in_array($type, $w4ils_default_types['cat'])){
+			//Get category by id
+			$cat = get_term_by( 'id', $id, 'category' );
 	
-	if($type == 'cat'){
-		//Get category by id
-		$cat = get_term_by( 'id', $id, 'category' );
-
-		//Get category by name
-		if(!$cat->term_id)
-			$cat = get_term_by( 'name', $name, 'category' );
-
-		//Get category by slug
-		if(!$cat->term_id)
-			$cat = get_term_by( 'slug', $slug, 'category' );
-
-			$id = $cat->term_id ;
-			if(!$id)
-				return false ;
-
-		$link = get_category_link($id);
-		if(!$text)	$text = get_cat_name($id);
-	}
+			//Get category by name
+			if(!$cat->term_id)
+				$cat = get_term_by( 'name', $name, 'category' );
 	
-	if($type == 'author'){
-		if(!get_userdata($id)){
-			//Get author by display name
-			$author = w4ils_get_author_by_field($name);
-			
-			//Get author by nicename
-			if($author)
-				$author = w4ils_get_author_by_field($slug, 'user_nicename');
-
-			$id = $author->ID;
-			if(!$id)
-				return false ;
+			//Get category by slug
+			if(!$cat->term_id)
+				$cat = get_term_by( 'slug', $slug, 'category' );
+	
+				$id = $cat->term_id ;
+				if(!$id)
+					return false ;
+	
+			$link = get_category_link($id);
+			if(!$text)
+				$text = get_cat_name($id);
 		}
-		$link = get_author_posts_url($id);
-		if(!$text)	$text = get_the_author_meta('display_name', $id);
+		
+		if(in_array($type, $w4ils_default_types['author'])){
+			if(!get_userdata($id)){
+				//Get author by display name
+				$author = $this->get_author_by_field($name);
+				
+				//Get author by nicename
+				if($author)
+					$author = $this->get_author_by_field($slug, 'user_nicename');
+	
+				$id = $author->ID;
+				if(!$id)
+					return false ;
+			}
+			$link = get_author_posts_url($id);
+
+			if(!$text)
+				$text = get_the_author_meta('display_name', $id);
+		}
+		
+		
+		$this->w4il = sprintf(__('<a class="%3$s" href="%1$s">%2$s</a>'), $link, $text, $class);
+		if($before)
+			$this->w4il = $before.$this->w4il;
+		
+		if($after)
+			$this->w4il = $this->w4il.$after;
+
+		return $this->w4il;
+		return false ;
 	}
 
-	if($link != '' && $text != '')
-		return sprintf(__('<a class="%3$s" href="%1$s">%2$s</a>'), $link, $text, $class);
-	return false ;
+
+	//Retrive and Replace shortcode
+	function w4ils_replace_callback($text){
+		$pattern = '/\[\s*intlink\s*(.*?)\s*\]/sm' ;
+		return preg_replace_callback( $pattern, array(&$this, 'w4ils_replace'), $text ) ;
+	}
 	
-}
+	//Custom post query function
+	function get_post_by_field($val = '', $key = 'post_title', $output = OBJECT) {
+		global $wpdb;
+			$post = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE $key = %s AND post_type IN ('post','page') AND post_status = 'publish' LIMIT 1", $val ));
+			if ( $post )
+				return get_post($post, $output);
+	
+		return false;
+	}
+	
+	//Custom author query function
+	function get_author_by_field($val = '', $key = 'display_name', $output = OBJECT) {
+		global $wpdb;
+			$author_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->users WHERE $key = %s LIMIT 1", $val ));
+			if ( $author_id )
+				return get_userdata($author_id);
+	
+		return false;
+	}
 
-//Retrive and Replace shortcode
-function w4ils_replace_callback($text){
-	$pattern = '/\[\s*intlink\s*(.*?)\s*\]/sm' ;
-	return preg_replace_callback( $pattern, 'w4ils_replace', $text ) ;
-}
-
-//Custom post query function
-function w4ils_get_post_by_field($val = '', $key = 'post_title', $output = OBJECT) {
-    global $wpdb;
-        $post = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE $key = %s AND post_type IN ('post','page') AND post_status = 'publish' LIMIT 1", $val ));
-        if ( $post )
-            return get_post($post, $output);
-
-    return false;
-}
-
-//Custom author query function
-function w4ils_get_author_by_field($val = '', $key = 'display_name', $output = OBJECT) {
-    global $wpdb;
-        $author_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->users WHERE $key = %s LIMIT 1", $val ));
-        if ( $author_id )
-            return get_userdata($author_id);
-
-    return false;
-}
+}//class W4ILS ends
+$W4ILS = new W4ILS();
 ?>
